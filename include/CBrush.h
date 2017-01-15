bd1a193e42a742f1d56c58feb1df1cabbd2e7063
Colin Williams <colinw@nc.rr.com>
2017-01-15 18:45:53 -0500

    new files

---
+++ include/CBrush.h
@@ -0,0 +1,86 @@
+#ifndef CBRUSH_H
+#define CBRUSH_H
+
+#include <CRGBA.h>
+#include <CBrushStyle.h>
+#include <CFillType.h>
+#include <CImage.h>
+#include <CAutoPtr.h>
+#include <CGenGradient.h>
+
+enum CBrushPattern {
+  CBRUSH_PATTERN_NONE,
+  CBRUSH_PATTERN_HORIZONTAL,
+  CBRUSH_PATTERN_VERTICAL,
+  CBRUSH_PATTERN_CROSS,
+  CBRUSH_PATTERN_DIAGONAL_UP,
+  CBRUSH_PATTERN_DIAGONAL_DOWN,
+  CBRUSH_PATTERN_DIAGONAL_CROSS,
+  CBRUSH_PATTERN_DOTTED1,
+  CBRUSH_PATTERN_DOTTED2,
+  CBRUSH_PATTERN_DOTTED3,
+  CBRUSH_PATTERN_DOTTED4,
+  CBRUSH_PATTERN_DOTTED5,
+  CBRUSH_PATTERN_DOTTED6,
+  CBRUSH_PATTERN_DOTTED7
+};
+
+class CBrush {
+ public:
+  typedef CRefPtr<CGenGradient> GradientPtr;
+
+ public:
+  CBrush() { }
+
+  explicit CBrush(const CRGBA &c) {
+    style_ = CBRUSH_STYLE_SOLID;
+    color_ = c;
+  }
+
+  explicit CBrush(const CImagePtr &image) {
+    style_   = CBRUSH_STYLE_TEXTURE;
+    texture_ = image;
+  }
+
+  CBrush &operator=(const CBrush &brush) {
+    return copy(brush);
+  }
+
+  CBrush &copy(const CBrush &brush) {
+    if (&brush == this)
+      return *this;
+
+    style_     = brush.style_;
+    color_     = brush.color_;
+    fill_rule_ = brush.fill_rule_;
+    pattern_   = brush.pattern_;
+    texture_   = brush.texture_;
+    gradient_  = brush.gradient_;
+
+    return *this;
+  }
+
+  CBrushStyle   getStyle   () const { return style_    ; }
+  const CRGBA&  getColor   () const { return color_    ; }
+  CFillType     getFillRule() const { return fill_rule_; }
+  CBrushPattern getPattern () const { return pattern_  ; }
+  CImagePtr     getTexture () const { return texture_  ; }
+  GradientPtr   getGradient() const { return gradient_ ; }
+
+  void setStyle   (CBrushStyle style    ) { style_     = style; }
+  void setColor   (const CRGBA &color   ) { color_     = color; }
+  void setFillRule(CFillType rule       ) { fill_rule_ = rule; }
+  void setPattern (CBrushPattern pattern) { pattern_   = pattern; }
+  void setTexture (CImagePtr texture    ) { texture_   = texture; }
+  void setGradient(GradientPtr gradient ) { gradient_  = gradient; }
+
+ private:
+  CBrushStyle   style_ { CBRUSH_STYLE_SOLID };
+  CRGBA         color_ { 0, 0, 0 };
+  CFillType     fill_rule_ { FILL_TYPE_EVEN_ODD };
+  CBrushPattern pattern_ { CBRUSH_PATTERN_HORIZONTAL };
+  CImagePtr     texture_;
+  GradientPtr   gradient_;
+};
+
+#endif
---
+++ include/CPen.h
@@ -0,0 +1,139 @@
+#ifndef CPEN_H
+#define CPEN_H
+
+#include <CRGBA.h>
+#include <CLineDash.h>
+#include <CILineDash.h>
+#include <CLineCapType.h>
+#include <CLineJoinType.h>
+
+class CPen {
+ public:
+  CPen() {
+    init();
+  }
+
+  CPen(const CRGBA &c) :
+   color_(c) {
+    init();
+  }
+
+  CPen(const CPen &pen) {
+    init();
+
+    copy(pen);
+  }
+
+  CPen &operator=(const CPen &pen) {
+    return copy(pen);
+  }
+
+  void init() {
+    dash_.init();
+  }
+
+  CPen &copy(const CPen &pen) {
+    if (&pen == this)
+      return *this;
+
+    color_       = pen.color_;
+    width_       = pen.width_;
+    dash_        = pen.dash_;
+    cap_         = pen.cap_;
+    join_        = pen.join_;
+    mitre_limit_ = pen.mitre_limit_;
+    flatness_    = pen.flatness_;
+
+    return *this;
+  }
+
+  const CRGBA     &getColor     () const { return color_      ; }
+  double           getWidth     () const { return width_      ; }
+  const CLineDash &getLineDash  () const { return dash_       ; }
+  CLineCapType     getLineCap   () const { return cap_        ; }
+  CLineJoinType    getLineJoin  () const { return join_       ; }
+  double           getMitreLimit() const { return mitre_limit_; }
+  double           getFlatness  () const { return flatness_   ; }
+
+  void setColor     (const CRGBA     &color   ) { color_       = color; }
+  void setWidth     (double           width   ) { width_       = width; }
+  void setLineDash  (const CLineDash &dash    ) { dash_        = dash; }
+  void setLineCap   (CLineCapType     cap     ) { cap_         = cap; }
+  void setLineJoin  (CLineJoinType    join    ) { join_        = join; }
+  void setMitreLimit(double           mitre   ) { mitre_limit_ = mitre; }
+  void setFlatness  (double           flatness) { flatness_    = flatness; }
+
+ private:
+  CRGBA         color_ { 0, 0, 0 };
+  double        width_ { 1.0 };
+  CLineDash     dash_;
+  CLineCapType  cap_ { LINE_CAP_TYPE_BUTT };
+  CLineJoinType join_ { LINE_JOIN_TYPE_MITRE };
+  double        mitre_limit_ { 0.0 };
+  double        flatness_ { 0.0 };
+};
+
+//-------
+
+class CIPen {
+ public:
+  CIPen() {
+    init();
+  }
+
+  CIPen(const CIPen &pen) {
+    init();
+
+    copy(pen);
+  }
+
+  CIPen &operator=(const CIPen &pen) {
+    return copy(pen);
+  }
+
+  void init() {
+    dash_.init();
+  }
+
+  CIPen &copy(const CIPen &pen) {
+    if (&pen == this)
+      return *this;
+
+    color_       = pen.color_;
+    width_       = pen.width_;
+    dash_        = pen.dash_;
+    cap_         = pen.cap_;
+    join_        = pen.join_;
+    mitre_limit_ = pen.mitre_limit_;
+    flatness_    = pen.flatness_;
+
+    return *this;
+  }
+
+  const CRGBA      &getColor     () const { return color_      ; }
+  uint              getWidth     () const { return width_      ; }
+  const CILineDash &getLineDash  () const { return dash_       ; }
+  CLineCapType      getLineCap   () const { return cap_        ; }
+  CLineJoinType     getLineJoin  () const { return join_       ; }
+  double            getMitreLimit() const { return mitre_limit_; }
+  double            getFlatness  () const { return flatness_   ; }
+
+  void setColor     (const CRGBA      &color   ) { color_       = color; }
+  void setWidth     (uint              width   ) { width_       = width; }
+  void setLineDash  (const CILineDash &dash    ) { dash_        = dash; }
+  void setLineCap   (CLineCapType      cap     ) { cap_         = cap; }
+  void setLineJoin  (CLineJoinType     join    ) { join_        = join; }
+  void setMitreLimit(double            mitre   ) { mitre_limit_ = mitre; }
+  void setFlatness  (double            flatness) { flatness_    = flatness; }
+
+ private:
+  CRGBA         color_ { 0, 0, 0 };
+  uint          width_ { 1 };
+  CILineDash    dash_;
+  CLineCapType  cap_ { LINE_CAP_TYPE_BUTT };
+  CLineJoinType join_ { LINE_JOIN_TYPE_MITRE };
+  double        mitre_limit_ { 0.0 };
+  double        flatness_ { 0.0 };
+};
+
+#endif
