e38cd61b501207ae0e6fa32a8877976aa8f000d2
Colin Williams <colinw@nc.rr.com>
2021-02-07 19:41:27 -0500

    new files

--- include/CAngleType.h
+++ include/CAngleType.h
@@ -7,4 +7,38 @@ enum CAngleType {
   CANGLE_TYPE_RADIANS
 };
 
+//---
+
+#include <algorithm>
+#include <cctype>
+#include <string>
+
+namespace CAngleTypeUtil {
+
+inline std::string toString(CAngleType type) {
+  if      (type == CANGLE_TYPE_DEGREES)
+    return "degrees";
+  else if (type == CANGLE_TYPE_RADIANS)
+    return "radians";
+  else
+    return "??";
+}
+
+inline CAngleType fromString(const std::string &str) {
+  std::string lstr = str;
+
+  std::transform(lstr.begin(), lstr.end(), lstr.begin(),
+    [](unsigned char c) { return std::tolower(c); }
+  );
+
+  if      (lstr == "degrees")
+    return CANGLE_TYPE_DEGREES;
+  else if (lstr == "radians")
+    return CANGLE_TYPE_RADIANS;
+  else
+    return CANGLE_TYPE_NONE;
+}
+
+}
+
 #endif
