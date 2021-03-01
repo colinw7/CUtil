fdd457b9ec483c7a72e52945a3baed3f20d5864f
Colin Williams <colinw@nc.rr.com>
2021-02-28 19:23:32 -0500

    new files

--- include/CHSV.h
+++ include/CHSV.h
@@ -131,10 +131,32 @@ class CHSV {
   void setSaturation(double s) { s_ = s; }
   void setValue     (double v) { v_ = v; }
 
+  uint getHueI       () const { return iround(h_); }
+  uint getSaturationI() const { return clampI(ivalue(s_)); }
+  uint getValueI     () const { return clampI(ivalue(v_)); }
+
   //CRGB toRGB() const;
 
  private:
-  double h_, s_, v_;
+  static uint clampI(int v) {
+    return (v >= 0 ? (v <= 255 ? v : 255) : 0);
+  }
+
+  static int iround(double r) {
+    if (r < 0)
+      return int(r - 0.5);
+    else
+      return int(r + 0.5);
+  }
+
+  static int ivalue(double r) {
+    return iround(r*255.0);
+  }
+
+ private:
+  double h_ { 0.0 };
+  double s_ { 0.0 };
+  double v_ { 0.0 };
 };
 
 //------
