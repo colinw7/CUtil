0b16be664169206bf91f7413b6b0bd9d7e266924
Colin Williams <colinw@nc.rr.com>
2022-04-11 20:17:12 -0400

    new files

---
+++ include/CSafeCast.h
@@ -0,0 +1,24 @@
+#ifndef CSafeCast_H
+#define CSafeCast_H
+
+#include <limits>
+#include <cstddef>
+#include <cassert>
+
+namespace CUtil {
+
+inline size_t toSizeT(int i) {
+  assert(i >= 0); return size_t(i);
+}
+
+inline double toDouble(int i) {
+  return double(i);
+}
+
+inline int toInt(size_t i) {
+  assert(i < std::numeric_limits<int>::max()); return int(i);
+}
+
+}
+
+#endif
--- include/CSafeIndex.h
+++ include/CSafeIndex.h
@@ -1,19 +1,21 @@
 #ifndef CSafeIndex_H
 #define CSafeIndex_H
 
+#include <CSafeCast.h>
+
 #include <vector>
 #include <cassert>
 
 namespace CUtil {
 
-template<typename T>
-const T &safeIndex(const std::vector<T> &v, int i) {
-  assert(i >= 0 && i < int(v.size())); return v[size_t(i)];
+template<typename T, typename INT=int>
+const T &safeIndex(const std::vector<T> &v, INT i) {
+  assert(i >= 0 && i < INT(v.size())); return v[size_t(i)];
 }
 
-template<typename T>
-T &safeIndex(std::vector<T> &v, int i) {
-  assert(i >= 0 && i < int(v.size())); return v[size_t(i)];
+template<typename T, typename INT=int>
+T &safeIndex(std::vector<T> &v, INT i) {
+  assert(i >= 0 && i < INT(v.size())); return v[size_t(i)];
 }
 
 }
